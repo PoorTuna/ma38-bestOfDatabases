94385b80681681cb5f4ea64b03a6340d1e7ac087
Oren <orengamingil@gmail.com>
2022-01-19 17:31:23 +0200

    Added basic table structure with empty functions

---
+++ src/main/java/database/tables/table.java
@@ -0,0 +1,24 @@
+package database.tables;
+
+import java.io.File;
+
+public class table {
+
+    private String path;
+    private File dataFile;
+    private File indexFile;
+    private File metaData;
+
+    public void insert(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void remove(){
+
+    }
+
+}
