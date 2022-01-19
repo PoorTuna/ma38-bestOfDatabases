f7521cb97e33e9e729a168ef28d5a10e13efb4b9
Oren <orengamingil@gmail.com>
2022-01-19 19:36:52 +0200

    Added documentation to table functions. Added function parameters.

--- src/main/java/database/tables/Table.java
+++ src/main/java/database/tables/Table.java
@@ -1,7 +1,6 @@
 package database.tables;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Table {
@@ -11,19 +10,41 @@ public class Table {
     private File indexFile;
     private File metaData;
 
-    public void insert(){
+    /**
+     * This function gets an object to insert, validates the contents, adds it to the table data and updates the metadata.
+     */
+    public <T> void insert(T obj) {
 
     }
 
-    public void update(){
+    /**
+     * This function takes a column id and data. The function checks input validity, changes the value in the table and updates the metadata.
+     */
+    public void update(String columnName, String value) {
 
     }
 
-    public void remove(){
+    /**
+     * This function takes a record primary key and checks if it exists in the index files. if it does it removes it from the records, metadata and index.
+     */
+    public void remove(String primaryKey) {
 
     }
 
-    public Map<String, String> select(){
+    /**
+     * This function takes a given primary key and returns the whole record if it finds it in the index file.
+     * @return
+     */
+    public Map<String, String> select(String columnName) {
         return null;
     }
+
+    /**
+     * This function takes the files and combines it into a single map which contains the table data.
+     * @return
+     */
+    public Map<String, String> selectAll() {
+        return null;
+    }
+
 }
